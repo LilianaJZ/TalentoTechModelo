ad9f713e92aad965ce3ef6425a7d9f3a23580394
LilianaJZ <ljimenezz@unal.edu.co>
2026-03-20 16:18:01 -0500

    Se añade configuración de información e imagen de la pestaña (frontend) en  main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import numpy as numpy
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#Configuración de info e imagen de la pestaña
+st.set_page_config(layout="centered",
+                    page_title="Talento Tech",
+                    page_icon=":bar_chart:")
